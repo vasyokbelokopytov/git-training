ce06682c5585e56a129357b8f9ceabf78b98df4b
Vasilii Bielokopytov <44782827+vasyokbelokopytov@users.noreply.github.com>
2022-02-21 17:01:16 +0200

    Create tasksSlice.tsx

---
+++ src/features/tasks/tasksSlice.tsx
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Task } from '../../app/types';
+
+interface TasksState {
+  tasks: Task[];
+}
+
+const initialState: TasksState = {
+  tasks: [],
+};
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    taskCreated: (state, { payload }: PayloadAction<string>) => {
+      state.tasks.push({
+        id: Date.now(),
+        completed: false,
+        text: payload,
+      });
+    },
+
+    taskRemoved: (state, { payload }: PayloadAction<number>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== payload);
+    },
+
+    completedChange: (state, { payload }: PayloadAction<number>) => {
+      state.tasks.forEach((task) => {
+        if (task.id === payload) {
+          task.completed = !task.completed;
+        }
+      });
+    },
+  },
+});
+
+export const { completedChange, taskCreated, taskRemoved } = tasksSlice.actions;
+export default tasksSlice.reducer;
