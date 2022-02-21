305c5e5c572f1a5fd80a7090dea7d2b132e6eab1
Vasilii Bielokopytov <44782827+vasyokbelokopytov@users.noreply.github.com>
2022-02-21 17:02:59 +0200

    Create Form.tsx

---
+++ src/components/Form.tsx
@@ -0,0 +1,33 @@
+import { Button, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { useAppDispatch } from '../app/hooks';
+import { taskCreated } from '../features/tasks/tasksSlice';
+
+export const Form: React.FC = () => {
+  const [taskText, setTaskText] = useState('');
+  const dispatch = useAppDispatch();
+
+  const clickHandler = () => {
+    if (taskText.trim() !== '') {
+      dispatch(taskCreated(taskText));
+      setTaskText('');
+    }
+  };
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskText(e.target.value);
+  };
+
+  return (
+    <Stack direction="row" spacing={2}>
+      <TextField
+        variant="standard"
+        sx={{ flexGrow: 1 }}
+        label="Your task"
+        value={taskText}
+        onChange={changeHandler}
+      />
+      <Button onClick={clickHandler}>Add task</Button>
+    </Stack>
+  );
+};
